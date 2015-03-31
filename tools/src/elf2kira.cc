6a568b54240cc0ed0f8a70f9441a2774a1d6463c
cagayakemiracl <hirosnonno1182@gmail.com>
2015-03-31 22:52:24 +0900

    とりあえずC++に

--- tools/src/elf2kira.cc
+++ tools/src/elf2kira.cc
@@ -1,17 +1,20 @@
+// Copyright (c) 2015 cagayakemiracl All Rights Reserved.
+// $Mail: <cagayakemiracl@gmail.com>
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <arpa/inet.h>
 #include <elf.h>
+#include <string>
 
-#define SIZE 1024 * 3  // h8 3694f ram 2kb
-#define INPUT "kiraos"
-#define OUTPUT "kiraos.kira"
+static constexpr auto SIZE = 1024 * 3;  // h8 3694f ram 2kb
+static constexpr auto INPUT = "kiraos";
+static constexpr auto OUTPUT = "kiraos.kira";
 
-static int elf_check(const Elf32_Ehdr *header) {
-  const int machine = ntohs(header->e_machine);
+static constexpr auto constexpr_elf_check(const Elf32_Ehdr *header) {
+  const auto machine = ntohs(header->e_machine);
 
-  if (memcmp((void *)header, ELFMAG, SELFMAG)) return -1;    // ELF file
   if (header->e_ident[EI_CLASS] != ELFCLASS32) return -1;    // ELF32
   if (header->e_ident[EI_DATA] != ELFDATA2MSB) return -1;    // Big endian
   if (header->e_ident[EI_VERSION] != EV_CURRENT) return -1;  // Version 1
@@ -23,14 +26,19 @@ static int elf_check(const Elf32_Ehdr *header) {
   return 0;
 }
 
-static int elf_extract_program(const Elf32_Ehdr *header, const FILE *row_file) {
-  const char d = 0;
-  int i;
+static auto elf_check(const Elf32_Ehdr *header) {
+  if (memcmp((void *)header, ELFMAG, SELFMAG)) return -1;    // ELF file
+  return constexpr_elf_check(header);
+}
 
-  for (i = 0; i < ntohs(header->e_shnum); i++) {
-    Elf32_Shdr *shdr = (Elf32_Shdr *)((char *)header + ntohl(header->e_shoff) +
-                                      ntohs(header->e_shentsize) * i);
-    int type = ntohl(shdr->sh_type);
+static constexpr auto elf_extract_program(const Elf32_Ehdr *header,
+                                          const FILE *row_file) {
+  constexpr char d = 0;
+
+  for (auto i = 0; i < ntohs(header->e_shnum); i++) {
+    const auto shdr = (Elf32_Shdr *)((char *)header + ntohl(header->e_shoff) +
+                                     ntohs(header->e_shentsize) * i);
+    const auto type = ntohl(shdr->sh_type);
 
     switch (type) {
       case SHT_NULL:
@@ -53,17 +61,18 @@ static int elf_extract_program(const Elf32_Ehdr *header, const FILE *row_file) {
   return -1;  // bss area does not exist file is abnormal
 }
 
-static void finalize_kira_write(const Elf32_Ehdr *header, const FILE *elf_file,
-                                const FILE *row_file) {
+static constexpr auto finalize_kira_write(const Elf32_Ehdr *header,
+                                          const FILE *elf_file,
+                                          const FILE *row_file) {
   free((void *)header);
   fclose((FILE *)elf_file);
   fclose((FILE *)row_file);
 }
 
-static int kira_write(const char *input, const char *output) {
-  const Elf32_Ehdr *header = (const Elf32_Ehdr *)malloc(SIZE);
-  const FILE *elf_file = fopen(input, "rb");
-  const FILE *row_file = fopen(output, "wb");
+static auto kira_write(const char *const input, const char *const output) {
+  const auto header = (const Elf32_Ehdr *)malloc(SIZE);
+  const auto elf_file = fopen(input, "rb");
+  const auto row_file = fopen(output, "wb");
 
   fread((void *)header, sizeof(char), SIZE, (FILE *)elf_file);
   if (elf_check(header) < 0) {
@@ -83,18 +92,10 @@ static int kira_write(const char *input, const char *output) {
   return 0;
 }
 
-int main(int argc, char **argv) {
-  char *input, *output;
-
-  if (argc == 3) {
-    input = argv[1];
-    output = argv[2];
-  } else {
-    input = INPUT;
-    output = OUTPUT;
-  }
+int main(void) {
+  constexpr auto input = INPUT, output = OUTPUT;
 
   printf("%s\n%s\n", input, output);
 
-  return kira_write((const char *)input, (const char *)output);
+  return kira_write(input, output);
 }
